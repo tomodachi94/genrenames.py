30012c50bbb3f9f4b0a7df9acadf1e30c69edc58
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-07-29 12:04:48 -0700

    Filter for valid extensions
    Prevents non-image files from getting into renames.txt.

--- src/cli.py
+++ src/cli.py
@@ -4,16 +4,15 @@ import os
 import renames  # local
 
 p = argparse.ArgumentParser(prog="genrenames-py",
-                            description="Utility to generate a renames.txt file.",
-                            epilog="""(c) Tomodachi94 2022, MIT License.
-                            A copy of the license should have been distributed with your copy of the software. If not, see https://mit-license.org .
+                            description="Utility to generate a renames.txt file.", epilog="""(c) Tomodachi94 2022, MIT License.
+A copy of the license should have been distributed with your copy of the software. If not, see https://mit-license.org .
 
-                            For more help, see `man genrenames`.
+For more help, see `man genrenames`.
                             """)
 
-p.add_argument("--path", "-p", metavar="PATH", type=str, help="The path to search.")
-p.add_argument("--output-file", "-o", metavar="PATH.txt", type=str, help="The file to dump output to. If blank, the program will dump to stdout.")
-p.add_argument("--list-all", "-a", action="store_true", type=bool, help="Adds all files in a directory to renames.txt.")
+p.add_argument("--path", "-p", metavar="PATH", help="The path to search.")
+p.add_argument("--output-file", "-o", metavar="PATH.txt", help="The file to dump output to. If blank, the program will dump to stdout.")
+p.add_argument("--list-all", "-a", action="store_true", help="Adds all files in a directory to renames.txt.")
 
 args = p.parse_args()
 
